94b0e800b60c84100fbc070321d257ba374b2f11
OmarSouza <73590497+OmarSouza@users.noreply.github.com>
2021-10-11 14:35:17 -0300

    Criação da Tela

--- src/Controller/Licenca_Controller.java
+++ src/Controller/Licenca_Controller.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Model.Licenca;
+
 public class Licenca_Controller {
     public Tratamento_De_Erro verificaDados(String veiculo_Placa_Cavalo, String veiculo_Placa_Reboque, String carga_Descricao, 
             String carga_Dimensao_Comprimento, String carga_Dimensao_Largura, String carga_Dimensao_Altura, 
@@ -29,5 +31,24 @@ public class Licenca_Controller {
             Tratamento_De_Erro retorno = new Tratamento_De_Erro("Campo 'Destino' não preenchido", false);
             return retorno;
         }
+        
+        //Tratamento Entrada Cliente
+        if(cliente_Cliente == null || cliente_Cliente.isEmpty()){
+            Tratamento_De_Erro retorno = new Tratamento_De_Erro("Campo 'Cliente' não preenchido", false);
+            return retorno;
+        }
+        
+        //Tratamento Entrada Nota Fiscal
+        if(cliente_Nota_Fiscal == null || cliente_Nota_Fiscal.isEmpty()){
+            Tratamento_De_Erro retorno = new Tratamento_De_Erro(("Campo 'Nota Fiscal' não preenchido"), false);
+            return retorno;
+        }
+        
+        Licenca licenca = new Licenca(veiculo_Placa_Cavalo, veiculo_Placa_Reboque, carga_Descricao, 
+                carga_Dimensao_Comprimento, carga_Dimensao_Largura, carga_Dimensao_Altura, 
+                carga_Dimensao_Peso, rota_Origem, rota_Destino, rota_Rota, cliente_Cliente, cliente_Nota_Fiscal);
+        
+        Tratamento_De_Erro retorno = new Tratamento_De_Erro("Solicitação Emitida com sucesso", true);
+        return retorno;
     }
 }
